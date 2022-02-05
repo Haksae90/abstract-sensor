b0714a684fa87517553d0323e4103630da5b9458
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-02-06 03:45:07 +0900

    finish assignment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,53 @@
-class Sensor {}
+class Sensor {
+    constructor(id) {
+        this.id = id;
+        this.powerStatus = 'off';
+        this.reportingInterval = 10000;
+    }
 
-class IotServer {}
+    turn(status) {
+        if (this.powerStatus === 'off' && status === 'on') {
+            this.powerStatus = 'on';
+            this.status = 'idle';
+
+            setTimeout(() => {
+                this.status = 'sensingDistance';
+            }, this.reportingInterval);
+            setTimeout(() => {
+                this.status = 'reportingData';
+            }, this.reportingInterval + 500);
+            setTimeout(() => {
+                this.status = 'idle';
+            }, this.reportingInterval + 1000);
+        } else if (status === 'off') {
+            this.powerStatus = 'off';
+        } else {
+            return error('status did not change');
+        }
+    }
+}
+
+class IotServer {
+    constructor() {
+        this.workingSensor = [];
+    }
+    
+    start(sensor) {
+        this.workingSensor.push(...sensor);
+    }
+
+    publish({ deviceId, actionId, payload }) {
+        for (const sensor of this.workingSensor) {
+            if (sensor.id === deviceId) {
+                if (sensor.powerStatus === 'on') {
+                    if (actionId === 'CHANGE_REPORTING_INTERVAL') {
+                        return (sensor.reportingInterval = payload);
+                    }
+                }
+            }
+        }
+    }
+}
 
 module.exports = {
     Sensor,
